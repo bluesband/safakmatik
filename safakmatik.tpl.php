0fb6123fa640d652a3f4b9a227a37deb08051b58
bluesband <giitar@gmail.com>
2012-07-06 17:39:43 +0300

    small changes

---
+++ safakmatik.tpl.php
@@ -0,0 +1,7 @@
+﻿   <div id="safakmatik">
+  <div><?php print t("Sülüs Tarihi : "); ?> <b> <?php print $variables["beginning"]; ?></b></div>
+  <div><?php print t("Bugün : "); ?> <b> <?php print $variables["today"]; ?></b></div>
+  <div><?php print t("Terhis Tarihi : "); ?> <b> <?php print $variables["end_date"]; ?></b></div>
+  <div><?php print t("Kalan Gün : "); ?> <b> <?php print $variables["remained_days"]; ?></b></div>
+  </div>
+  
